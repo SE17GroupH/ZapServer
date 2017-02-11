5d909b4f47c590fba8cae57e36d5fe0978402d96
Harold Chen <ychen71@ncsu.edu>
2017-02-10 20:45:11 -0500

    Set routes constraints

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+	#API definition
+	namespace :api, defaults: { format: :json },
+								constraints: { subdomain: 'api' }, path: '/' do
+		# add resources here
+	end
+
 end
