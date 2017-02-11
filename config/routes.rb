e337103103082727f27ca3c0fdf5c1d143dcaa6e
Harold Chen <ychen71@ncsu.edu>
2017-02-10 20:40:04 -0500

    Restart the whole project from scartch, another approach

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'home/index'
-
-  root 'home#index'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
