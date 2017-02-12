f1d95f8fb2766ee970bbee3a3cd52a2b182c6c27
Harold Chen <ychen71@ncsu.edu>
2017-02-12 12:28:11 -0500

    changing route file for Heroku deployment

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
 
   devise_for :users
 	#API definition
-	namespace :api, defaults: { format: :json },
-								constraints: { subdomain: 'api' }, path: '/' do
+	# namespace :api, defaults: { format: :json },
+	# 							constraints: { subdomain: 'api' }, path: '/' do
+	namespace :api, defaults: { format: :json }, path: '/api' do
 		scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
 
 		# add resources here
