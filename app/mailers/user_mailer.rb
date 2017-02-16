aa187d8279d6464ce75f941f5534be62cb99609e
Harold Chen <ychen71@ncsu.edu>
2017-02-16 00:07:36 -0500

    add dubug funtion

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,17 +1,9 @@
 class UserMailer < ApplicationMailer
-
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.new_session.subject
-
-  #
-
   default from: "zapse17@gmail.com"
-
 
   def new_session(user)
     @user = user
+    logger.debug user.email
     mail to: user.email, subject: "Your Magic Link to Log In"
   end
 end
