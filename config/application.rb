f8b76e9332572a3333c1ba08432a51fc1c155dbf
Harold Chen <ychen71@ncsu.edu>
2017-02-15 23:02:30 -0500

    fix dotenv error in application.rb

--- config/application.rb
+++ config/application.rb
@@ -5,10 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-Dotenv::Railtie.load
 
-MAIL_USER = 'zapse17@gmail.com'
-MAIL_PASSWORD = 'chenyujia930223'
 
 module ZapServer
   class Application < Rails::Application
