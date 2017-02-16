168481eee5eee4f2af6d140562edfb58a62a37ac
Harold Chen <ychen71@ncsu.edu>
2017-02-16 08:58:12 -0500

    modify sending mailer controller method

--- app/controllers/api/v1/magiclinks_controller.rb
+++ app/controllers/api/v1/magiclinks_controller.rb
@@ -5,8 +5,8 @@ class Api::V1::MagiclinksController < ApplicationController
     user_email = params[:magiclink][:email]
     user = user_email.present? && User.find_by(email: user_email)
     if user 
-      UserMailer.new_session(user)
-      render json: user, status: 200, location: [:api, user]
+      UserMailer.new_session(user).deliver_now
+      # render json: user, status: 200, location: [:api, user]
     else
       render json: { errors: "No accounts found!" }, status: 422
     end
